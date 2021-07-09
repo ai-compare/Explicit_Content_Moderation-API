1033dde4b888d400f1b8373acb852c3dd961c65b
Eden AI <67952486+ai-compare@users.noreply.github.com>
2021-07-09 15:03:04 +0200

    Update AI-Compare-Explicit_Content_Moderation-python-request.py

--- AI-Compare-Explicit_Content_Moderation-python-request.py
+++ AI-Compare-Explicit_Content_Moderation-python-request.py
@@ -1,20 +1,20 @@
 import requests
 
-# You can find the documentation here : https://www.ai-compare.com/v1/redoc/#operation/Explicit%20Content%20Detection
+# You can find the documentation here : https://api.edenai.run/v1/redoc/#section/How-it-works
 
 #Enter your API Token
-headers = {  'Authorization': 'Bearer your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
+headers = {  'Authorization': 'Bearer your API Key'}
 
 # Select API
-url = 'https://www.ai-compare.com/v1/pretrained/vision/explicit_content_detection'
+url = 'https://api.edenai.run/v1/pretrained/vision/explicit_content_detection'
 
 # Select providers
-payload = {'providers': '[\'google_cloud\', \'cognitives_service\', \'aws\', \'ibm\']'}
+payload = {'providers': '[\'google_cloud\', \'microsoft\', \'aws\', \'ibm\']'}
 
 # Select file to test
 files = [  ('files', open('Picture/example.jpg','rb'))]
 
-# Request to AI COMPARE
+# Request to Eden AI
 response = requests.request("POST", url, headers=headers, data = payload, files = files)
 
 # Print result
